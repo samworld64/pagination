a08574b34b0d0ecd94208e4768b7f5b502f92706
Semere Berhane <samworld64@gmail.com>
2022-09-12 23:07:17 +0300

    exit system

--- src/main/java/sam/paginate/world.java
+++ src/main/java/sam/paginate/world.java
@@ -47,6 +47,7 @@ public class world extends javax.swing.JFrame {
         btnLast = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -105,6 +106,15 @@ public class world extends javax.swing.JFrame {
         });
 
         jMenu1.setText("File");
+
+        jMenuItem1.setText("Exit");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Edit");
@@ -181,6 +191,10 @@ public class world extends javax.swing.JFrame {
         loadWorldtbl();
     }//GEN-LAST:event_cmbLimitActionPerformed
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+       System.exit(0);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
     private void loadWorldtbl(){
         DefaultTableModel dt = (DefaultTableModel) worldtbl.getModel();
         dt.setRowCount(0);
@@ -219,6 +233,7 @@ public class world extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable worldtbl;
     // End of variables declaration//GEN-END:variables
